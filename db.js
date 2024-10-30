32b3721e7f40cac4613c9eecf3f16bcb4f01bb05
Thamila92 <achatthamila@gmail.com>
2024-10-30 11:05:22 +0100

    add database to project

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
  
 // Database
 const sequelize = new Sequelize(
-  perocess.env.DB_URL, // TODO: database connection string
+  process.env.DB_URL, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
